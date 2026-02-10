2c11ba89e9bd35619684708caf0ab6d3ad834a8d
divakr-rdk19 <divakarraj49211@gmail.com>
2026-02-10 22:44:00 +0530

    basic setup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,26 @@
 import { Stack } from "expo-router";
 import { StatusBar } from "react-native";
+import { useFonts } from "expo-font";
 import "./global.css";
 
 export default function RootLayout() {
+  const [loaded] = useFonts({
+    "Jakarta-Bold": require("../assets/fonts/PlusJakartaSans-Bold.ttf"),
+    "Jakarta-ExtraBold": require("../assets/fonts/PlusJakartaSans-ExtraBold.ttf"),
+    "Jakarta-ExtraLight": require("../assets/fonts/PlusJakartaSans-ExtraLight.ttf"),
+    "Jakarta-Light": require("../assets/fonts/PlusJakartaSans-Light.ttf"),
+    "Jakarta-Medium": require("../assets/fonts/PlusJakartaSans-Medium.ttf"),
+    "Jakarta-Regular": require("../assets/fonts/PlusJakartaSans-Regular.ttf"),
+    "Jakarta-SemiBold": require("../assets/fonts/PlusJakartaSans-SemiBold.ttf"),
+  });
   return (
     <>
       <StatusBar hidden={true} />
       <Stack>
+        <Stack.Screen name="(root)" options={{ headerShown: false }} />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
       </Stack>
     </>
   );
 }
+
