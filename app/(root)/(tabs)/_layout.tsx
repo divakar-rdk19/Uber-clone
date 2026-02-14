c2794714708c7c695dc80c2387f50d0980f0663f
divakr-rdk19 <divakarraj49211@gmail.com>
2026-02-14 21:13:35 +0530

    Bottom Navigation bar completed

--- app/(root)/(tabs)/_layout.tsx
+++ app/(root)/(tabs)/_layout.tsx
@@ -1,13 +1,79 @@
-import { Stack, Tabs } from "expo-router";
+import { icons } from "@/constants";
+import { Tabs } from "expo-router";
+import { Image, ImageSourcePropType, View } from "react-native";
+
+const TabIcon = ({focused, source}: {focused: boolean, source: ImageSourcePropType}) =>(
+  <View className={`flex flex-row justify-center items-center rounded-full mt-10 ${focused && 'bg-general-300'}`}>
+    <View className={`rounded-full w-12 h-12 items-center justify-center ${focused && 'bg-general-400'}`}>
+      <Image source={source} className="w-7 h-7" tintColor="white" resizeMode="contain"/>
+    </View>
+  </View>
+)
 
 export default function TabLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="home" options={{ headerShown: false }} />
-      <Stack.Screen name="chat" options={{ headerShown: false }} />
-      <Stack.Screen name="profile" options={{ headerShown: false }} />
-      <Stack.Screen name="rides" options={{ headerShown: false }} />
-      <Stack.Screen name="explore" options={{ headerShown: false }} />
-    </Stack>
+    <Tabs
+      initialRouteName="home"
+      screenOptions={{
+        tabBarActiveTintColor: "white",
+        tabBarInactiveTintColor: "white",
+        tabBarShowLabel: false,
+        tabBarStyle:{
+          backgroundColor: "#333333",
+          borderRadius: 50,
+          overflow: "hidden",
+          marginHorizontal: 20,
+          marginBottom: 20,
+          height: 78,
+          position: "absolute"
+        },
+        tabBarItemStyle:{
+          justifyContent: "center",
+          alignItems: "center",
+          height: 78
+        }
+      }}
+    >
+      <Tabs.Screen
+        name="home"
+        options={{
+          title: "Home",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon focused={focused} source={icons.home} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="rides"
+        options={{
+          title: "Rides",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon focused={focused} source={icons.list} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="chat"
+        options={{
+          title: "Chat",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon focused={focused} source={icons.chat} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon focused={focused} source={icons.profile} />
+          ),
+        }}
+      />
+    </Tabs>
   );
 }
--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -1,13 +1,16 @@
 import CustomButton from "@/components/CustomButton";
-import { router } from "expo-router";
+import { SignedIn, SignedOut, useUser } from "@clerk/clerk-expo";
+import { Link, router } from "expo-router";
 import { Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function HomeScreen() {
+  const user = useUser();
   return (
     <SafeAreaView className="flex-1 items-center justify-center">
-      <Text>New Index</Text>
-      <CustomButton title="Logout" onPress={() => router.replace("/(auth)/welcome")}/>
+      <SignedIn>
+        <Text>Hello user</Text>
+      </SignedIn>
     </SafeAreaView>
   );
 }
